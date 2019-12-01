119bcbc6551bb63f8e57763d00da6e8041fe7f4a
Jesper Dramsch <jesper@dramsch.net>
2019-12-01 16:27:15 +0100

    Class it up

--- 2019/util.py
+++ 2019/util.py
@@ -1,4 +1,26 @@
-def load_aoc(day):
-    with open(f"day-{day}.txt") as f:
-        data = f.readlines() 
-    return data
+class Day:
+    def __init__(self, day: int, part: int):
+        from problems import description
+        self.day  = day
+        self.part = part
+        self.desc = description(day, part)
+    
+    def load(self, typing=str):
+        with open(f"day{self.day:02d}.txt") as f:
+            data = f.read().splitlines()
+        self.data = list(map(typing, data))
+        return self.data
+
+    def apply(self, func):
+        return list(map(func, self.data))
+
+    def answer(self, num):
+        return f"The Solution on Day {self.day} for Part {self.part} is: {num}"
+
+if __name__ == "__main__":
+    day = Day(1,1)
+
+    print("Data is:", day.data(int))
+    print("Day is:", day.day)
+    print("Part is:", day.part)
+    print("Description is:", day.desc)
