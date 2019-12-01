376fefd33b65157d7518167261ecbf4fe5c58541
Jesper Dramsch <jesper@dramsch.net>
2019-12-01 16:26:30 +0100

    Rename

---
+++ 2019/day01.py
@@ -0,0 +1,27 @@
+from util import Day
+
+def fuel(module: int):
+    return module // 3 - 2
+
+def fuelchain(module: int):
+    out = [fuel(module)]
+    while(out[-1] > 0):
+        out.append(fuel(out[-1]))
+    return sum(out[:-1])
+
+if __name__ == "__main__":
+    ## Part One
+    part1 = Day(1,1)
+    part1.load(int)
+    
+    out = sum(part1.apply(fuel))
+    
+    print(part1.answer(out))
+
+    ## Part 2
+    part2 = Day(1,2)
+    part2.load(int)
+
+    out = sum(part2.apply(fuelchain))
+    
+    print(part2.answer(out))
