0e310421e1b5d1a929d2ed746e03f849654d4d0a
Jesper Dramsch <jesper@dramsch.net>
2019-12-03 20:47:49 +0100

    Given Tests Part 1

--- 2019/tests/test_day1.py
+++ 2019/tests/test_day1.py
@@ -9,6 +9,30 @@ def test_part1():
     part1.load(int)
     assert sum(part1.apply(fuel)) == 3279287
 
+def test_part1_given_0():
+    part1 = Day(1,1)
+    part1.load(typing=int, data=[12])
+    part1.answer(sum(part1.apply(fuel)))
+    assert part1.result == 2
+
+def test_part1_given_1():
+    part1 = Day(1,1)
+    part1.load(typing=int, data=[14])
+    part1.answer(sum(part1.apply(fuel)))
+    assert part1.result == 2
+
+def test_part1_given_2():
+    part1 = Day(1,1)
+    part1.load(typing=int, data=[1969])
+    part1.answer(sum(part1.apply(fuel)))
+    assert part1.result == 654
+
+def test_part1_given_3():
+    part1 = Day(1,1)
+    part1.load(typing=int, data=[100756])
+    part1.answer(sum(part1.apply(fuel)))
+    assert part1.result == 33583
+
 def test_part2():
     part2 = Day(1,2)
     part2.load(int)
