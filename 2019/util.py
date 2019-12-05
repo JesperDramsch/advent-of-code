a8a8e9d3ad219b8f7d9adf80d3b8d9506a93f6a7
Jesper Dramsch <jesper@dramsch.net>
2019-12-05 10:24:44 +0100

    Add Opcode 3,4

--- 2019/util.py
+++ 2019/util.py
@@ -37,7 +37,9 @@ class Day:
         """Reset Data to original
         """
         self.data = self.raw_data.copy()
-
+    def sum(self) -> float:
+        return sum(self.data)
+    
     def apply(self, func, *args, **kwargs) -> list:
         """Apply a function to every element.
         Changes the original data.
@@ -51,33 +53,59 @@ class Day:
         mapfunc = partial(func, *args,**kwargs)
         self.data = list(map(mapfunc, self.data))
         return self.data
-
+    
     def execute_opcode(self) -> list:
         """Execute OpCode operation
         
         1:  Add
         2:  Multiply
+        3:  Input
+        4:  Output
         99: Exit
 
         Returns:
             list -- Opcode after execution
         """
-        for i in range(0,len(self.data),4):
-            if self.data[i] == 1:
-                self.data[self.data[i+3]] = self.data[self.data[i+1]] + self.data[self.data[i+2]]
-            elif self.data[i] == 2:
-                self.data[self.data[i+3]] = self.data[self.data[i+1]] * self.data[self.data[i+2]]
-            elif self.data[i] == 99:
+
+        def __opmode(position: int, mode: int) -> int:
+            if int(mode) == 0:
+                return self.data[position]
+            elif int(mode) == 1:
+                return position
+        
+        def __instructor(code: int):
+            mode = f"{code:05d}"
+            return int(mode[3:]), mode[2], mode[1], mode[0]
+
+        loop = 0
+        while loop < len(self.data) or self.data[loop] != 99:
+            instruct, A, B, C = __instructor(self.data[loop])
+            if instruct == 1:
+                self.data[__opmode(loop+3, C)] = self.data[__opmode(loop+1, A)] + self.data[__opmode(loop+2, B)]
+                inc = 4
+            elif instruct == 2:
+                self.data[__opmode(loop+3, C)] = self.data[__opmode(loop+1, A)] * self.data[__opmode(loop+2, B)]
+                inc = 4
+            elif instruct == 3:
+                self.data[__opmode(loop+1, C)] = int(input("Please provide input: "))
+                inc = 2
+            elif instruct == 4:
+                self.diagnostic = self.data[__opmode(loop+1, C)]
+                print(self.diagnostic)
+                inc = 2
+            elif instruct == 99:
                 return self.data 
             else:
                 break
+            loop += inc
     
-    def sum(self) -> float:
-        return sum(self.data)
-
-    def answer(self, num) -> str:
-        self.result = num
-        return f"The Solution on Day {self.day} for Part {self.part} is: {num}"
+    def answer(self, num=None, v=False) -> str:
+        if not num == None:
+            self.result = num
+        s = f"The Solution on Day {self.day} for Part {self.part} is: {self.result}"
+        if v:
+            print(s)
+        return s
 
 if __name__ == "__main__":
     day = Day(1,1)
