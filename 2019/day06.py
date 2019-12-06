556ea5c6f8e914a20dc4606ad5077e4f4920a9cc
Jesper Dramsch <jesper@dramsch.net>
2019-12-06 09:39:15 +0100

    Day 6 [🌟🌟]

--- 2019/day06.py
+++ 2019/day06.py
@@ -1,13 +1,17 @@
 from util import Day
 
 def get_endpoints(pairs: list) -> list:
+    list_pairs = pairs.copy()
     x, y = zip(*pairs)
     return list(set(y)-set(x))
 
 def get_chain(pairs:list) -> dict:
-    return {y: (x if x != "COM" else 1) for x, y in pairs}
+    list_pairs = pairs.copy()
+    return {y: (x if x != "COM" else 1) for x, y in list_pairs}
 
-def traverse_orbits(chain: dict, end_points: list) -> int:
+def traverse_orbits(all_orbits: dict, end_points: list): 
+    chain = all_orbits.copy()
+    list_points = end_points.copy()
     orbits = 1
     for point in end_points:
         node = point
@@ -20,15 +24,24 @@ def traverse_orbits(chain: dict, end_points: list) -> int:
             sub_orbits.append(i)
             node = chain[node]
             i += 1
-        sub_orbits = [node + x for x in sub_orbits] # add rest of chain from stop node
+        sub_orbits = [node + x for x in sub_orbits] # add rest of chains from stop node
         orbits += sum(sub_orbits[1:])
-        
-        # Populate chain with node values and mark them visited
+        #print(sub_orbits)
+        # Populate chains with node values and mark them visited
         for j, el in enumerate(nodes[::-1]):
             chain[el] = j + node
+    [chain.pop(k) for k in all_orbits if type(chain[k]) != int]
+    return orbits, chain
 
-    return(orbits)
+def center_search(orbits: list, end_one: str, end_two:str):
+    chain = orbits.copy()
+    center_one, one = traverse_orbits(chain, [end_one])
+    center_two, two = traverse_orbits(chain, [end_two])
 
+    common = set(one.keys()) & set(two.keys())
+    closest = max(one[x] for x in common)
+    
+    return one[end_one]-1 + two[end_two]-1 - closest * 2
 
 if __name__ == "__main__":
     part1 = Day(6,1)
@@ -37,6 +50,16 @@ if __name__ == "__main__":
     
     chain = get_chain(part1.data)
     end_points = get_endpoints(part1.data)
-    
-    part1.answer(traverse_orbits(chain, end_points), v=True)
+    orbits, traverse = traverse_orbits(chain, end_points)
+    part1.answer(orbits, v=True)
 
+    part2 = Day(6,2)
+    part2.load()
+    part2.apply(str.split, sep=")")
+
+    chain = get_chain(part2.data)
+    
+    part2.answer(center_search(chain, "SAN", "YOU"), v=True)
+
+    
+    
--- 2019/tests/test_day6.py
+++ 2019/tests/test_day6.py
@@ -21,8 +21,8 @@ K)L""".split())
     
     chain = get_chain(part1.data)
     end_points = get_endpoints(part1.data)
-    
-    part1.answer(traverse_orbits(chain, end_points), v=True)
+    orbits, _ = traverse_orbits(chain, end_points)
+    part1.answer(orbits)
 
     assert part1.result == 42
 
@@ -34,6 +34,42 @@ def test_part1():
     chain = get_chain(part1.data)
     end_points = get_endpoints(part1.data)
     
-    part1.answer(traverse_orbits(chain, end_points), v=True)
+    orbits, _ = traverse_orbits(chain, end_points)
+
+    part1.answer(orbits)
 
     assert part1.result == 315757
+
+def test_given2():
+    part2 = Day(6,2)
+    part2.load("""COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN""".split())
+    part2.apply(str.split, sep=")")
+    
+    chain = get_chain(part2.data)
+    
+    part2.answer(center_search(chain, "SAN", "YOU"))
+
+    assert part2.result == 4
+
+def test_part2():
+    part2 = Day(6,2)
+    part2.load()
+    part2.apply(str.split, sep=")")
+
+    chain = get_chain(part2.data)
+    
+    part2.answer(center_search(chain, "SAN", "YOU"))
+
+    assert part2.result == 481
