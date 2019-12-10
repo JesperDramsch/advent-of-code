d180d6c9e6e5218fd02a58e87e46559a31a1f8a9
Jesper Dramsch <jesper@dramsch.net>
2019-12-10 20:54:54 +0100

    6s -> .3s

--- 2019/day10.py
+++ 2019/day10.py
@@ -2,62 +2,54 @@ from util import Day
 
 
 def process_map(data):
-    out_data = {}
+    out_data = set()
     for i, el in enumerate(data):
         for j, val in enumerate(el.strip()):
             if val == "#":
-                out_data[(j, i)] = True
+                out_data.add((j, i))
     return out_data
-
-
-def shadow(data, center: tuple, obj: tuple):
-
-    cx, cy = center
-    ox, oy = obj
-
-    masks = data.copy()
-    if center == obj:
-        masks.pop((cx, cy), None)
-        return masks
-
-    ix, iy = ox - cx, oy - cy
-
-    # Find the greatest common denominator
-    for ii in range(10, 1, -1):
-        if ix % ii == 0 and iy % ii == 0:
-            ix = ix // ii
-            iy = iy // ii
-
-    x, y = zip(*data.keys())
-    max_x, max_y = max(x), max(y)
-
-    dx = ox + ix
-    dy = oy + iy
-
-    while 0 <= dx <= max_x and 0 <= dy <= max_y:
-        masks.pop((dx, dy), None)
-        dx += ix
-        dy += iy
-    return masks
 
 
 def all_shadows(data, center, asteroids):
 
-    masks = data.copy()
+    masks = set()
+    cx, cy = center
+
+    x, y = zip(*data)
+    max_x, max_y = max(x), max(y)
 
     for asteroid in asteroids:
-        masks = shadow(masks, center, asteroid)
+        ox, oy = asteroid
 
-    return masks
+        if center == asteroid:
+            masks.add((cx, cy))
+        else:
+            ix, iy = ox - cx, oy - cy
+
+            # Find the smallest vector
+            for ii in range(10, 1, -1):
+                if ix % ii == 0 and iy % ii == 0:
+                    ix = ix // ii
+                    iy = iy // ii
+
+            dx = ox + ix
+            dy = oy + iy
+
+            while 0 <= dx <= max_x and 0 <= dy <= max_y:
+                if (dx, dy) in data:
+                    masks.add((dx, dy))
+                dx += ix
+                dy += iy
+
+    return data - masks
 
 
 def count_visible(data):
 
-    star_map = data.copy()
-    # print(data)
+    star_map = {}
 
-    for asteroid in data.keys():
-        star_map[asteroid] = len(all_shadows(data, asteroid, data.keys()))
+    for asteroid in data:
+        star_map[asteroid] = len(all_shadows(data, asteroid, data))
 
     new_base = max(star_map, key=star_map.get)
 
@@ -68,9 +60,10 @@ def wazer_wifle(data, center, num):
     from math import atan2
 
     cx, cy = center
+    seen = all_shadows(data, center, data)
 
-    seen = all_shadows(data, center, data.keys())
-
+    return sorted(((atan2((x - cx), (y - cy)), (x, y)) for x, y in seen), reverse=True)[num - 1][1]
+    # First try
     # rotate = [(x, 0) for x in range(cx, max_x)] # center x out at y=0
     # rotate.extend([(max_x-1, y) for y in range(1,max_y)]) # down y
     # rotate.extend([(x, max_y-1) for x in range(max_x-2, -1, -1)]) # back in x
@@ -78,170 +71,4 @@ def wazer_wifle(data, center, num):
     # rotate.extend([(x,0) for x in range(1, cx)]) # back to start
 
     # Nice idea, if it sampled everything. Let's go polar!
-
-    return sorted(((atan2((x - cx), (y - cy)), (x, y)) for x, y in seen.keys()), reverse=True)[
-        num - 1
-    ][1]
-
-
-if __name__ == "__main__":
-
-    part2 = Day(10, 2)
-    raw_data = """.#....#####...#..
-##...##.#####..##
-##...#...#.#####.
-..#.....X...###..
-..#.#.....#....##""".split(
-        "\n"
-    )
-    part2.load(raw_data, sep="\n")
-    part2.data = process_map(part2.data)
-    print(wazer_wifle(part2.data, (8, 4), 3))
-
-    raw_data = """.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##""".split(
-        "\n"
-    )
-
-    part2.load(raw_data, sep="\n")
-    part2.data = process_map(part2.data)
-
-    shtuff = [1, 2, 3, 10, 20, 50, 100, 199, 200, 201]
-
-    for el in shtuff:
-        print(f"The {el} asteroid to be  vaporized is at: ", wazer_wifle(part2.data, (11, 13), el))
-
-    part2.load(sep="\n")
-
-    part2.data = process_map(part2.data)
-
-    station = count_visible(part2.data)[1]
-    print(station)
-    part2.answer(wazer_wifle(part2.data, station, 200), v=2)
-
-    print(part2.result[0] * 100 + part2.result[1])
-
-#     part1 = Day(10, 1)
-
-#     raw_data = """.#..#
-# .....
-# #####
-# ....#
-# ...##""".split()
-
-#     part1.load(raw_data, sep="\n")
-
-#     part1.data = process_map(part1.data)
-
-#     #print(part1.data)
-
-#     print(all_shadows(part1.data, (0, 1), part1.data.keys()))
-
-
-#     print(shadow(part1.data, (0, 1), (0,2)))
-
-#     part1.answer(count_visible(part1.data)[0], v=1)
-
-#     raw_data = """......#.#.
-# #..#.#....
-# ..#######.
-# .#.#.###..
-# .#..#.....
-# ..#....#.#
-# #..#....#.
-# .##.#..###
-# ##...#..#.
-# .#....####""".split()
-
-#     part1.load(raw_data, sep="\n")
-
-#     part1.data = process_map(part1.data)
-
-#     part1.answer(count_visible(part1.data)[0], v=1)
-
-#     raw_data = """#.#...#.#.
-# .###....#.
-# .#....#...
-# ##.#.#.#.#
-# ....#.#.#.
-# .##..###.#
-# ..#...##..
-# ..##....##
-# ......#...
-# .####.###.""".split()
-
-#     part1.load(raw_data, sep="\n")
-
-#     part1.data = process_map(part1.data)
-
-#     part1.answer(count_visible(part1.data)[0], v=1)
-
-#     raw_data = """.#..#..###
-# ####.###.#
-# ....###.#.
-# ..###.##.#
-# ##.##.#.#.
-# ....###..#
-# ..#.#..#.#
-# #..#.#.###
-# .##...##.#
-# .....#.#..""".split()
-
-#     part1.load(raw_data, sep="\n")
-
-#     part1.data = process_map(part1.data)
-
-#     part1.answer(count_visible(part1.data)[0], v=1)
-
-
-#     raw_data = """.#..##.###...#######
-# ##.############..##.
-# .#.######.########.#
-# .###.#######.####.#.
-# #####.##.#.##.###.##
-# ..#####..#.#########
-# ####################
-# #.####....###.#.#.##
-# ##.#################
-# #####.##.###..####..
-# ..######..##.#######
-# ####.##.####...##..#
-# .#####..#.######.###
-# ##...#.##########...
-# #.##########.#######
-# .####.#.###.###.#.##
-# ....##.##.###..#####
-# .#.#.###########.###
-# #.#.#.#####.####.###
-# ###.##.####.##.#..##""".split()
-
-# part1.load(raw_data, sep="\n")
-
-# part1.data = process_map(part1.data)
-
-# part1.answer(count_visible(part1.data)[0], v=1)
-
-# part1.load(sep="\n")
-
-# part1.data = process_map(part1.data)
-
-# part1.answer(count_visible(part1.data)[0], v=1)
 
--- 2019/tests/test_day10.py
+++ 2019/tests/test_day10.py
@@ -6,36 +6,36 @@ from util import Day
 from day10 import *
 
 
-@pytest.mark.parametrize(
-    "center,obj,mask",
-    [
-        ([3, 2], [3, 2], [(3, 2)]),
-        ([1, 2], [2, 2], [(3, 2), (4, 2)]),
-        ([0, 0], [1, 1], [(2, 2), (3, 3), (4, 4)]),
-        ([0, 0], [3, 3], [(4, 4)]),
-        ([0, 0], [2, 2], [(3, 3), (4, 4)]),
-        ([2, 2], [1, 1], [(0, 0)]),
-        ([4, 4], [3, 2], [(2, 0)]),
-    ],
-)
-def test_shadow_masks(center, obj, mask):
-    part1 = Day(10, 1)
+# @pytest.mark.parametrize(
+#     "center,obj,mask",
+#     [
+#         ([3, 2], [3, 2], [(3, 2)]),
+#         ([1, 2], [2, 2], [(3, 2), (4, 2)]),
+#         ([0, 0], [1, 1], [(2, 2), (3, 3), (4, 4)]),
+#         ([0, 0], [3, 3], [(4, 4)]),
+#         ([0, 0], [2, 2], [(3, 3), (4, 4)]),
+#         ([2, 2], [1, 1], [(0, 0)]),
+#         ([4, 4], [3, 2], [(2, 0)]),
+#     ],
+# )
+# def test_shadow_masks(center, obj, mask):
+#     part1 = Day(10, 1)
 
-    raw_data = """.#..#
-.....
-#####
-....#
-...##""".split()
+#     raw_data = """.#..#
+# .....
+# #####
+# ....#
+# ...##""".split()
 
-    part1.load(raw_data, sep="\n")
+#     part1.load(raw_data, sep="\n")
 
-    part1.data = process_map(part1.data)
+#     part1.data = process_map(part1.data)
 
-    comp_data = part1.data.copy()
-    for keys in mask:
-        comp_data.pop(keys, None)
+#     comp_data = part1.data.copy()
+#     for keys in mask:
+#         comp_data.pop(keys, None)
 
-    assert comp_data == shadow(part1.data, center, obj)
+#     assert comp_data == shadow(part1.data, center, obj)
 
 
 @pytest.mark.parametrize(
@@ -57,7 +57,7 @@ def test_shadow_all(center, obj, mask):
 
     comp_data = part1.data.copy()
     for keys in mask:
-        comp_data.pop(keys, None)
+        comp_data.discard(keys)
     assert comp_data == all_shadows(part1.data, center, obj)
 
 
