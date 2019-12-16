98cf040ee7813465f253fecbf34dc2111dbf333c
Jesper Dramsch <jesper@dramsch.net>
2019-12-16 18:31:32 +0100

    Numpy for animation

--- 2019/day13.py
+++ 2019/day13.py
@@ -15,10 +15,11 @@ class Arcade(Day):
         
         
     def paint(self):
-        self.canvas = {}
+        self.canvas = np.zeros((43,23,12000))
+        self.scores = np.zeros((12000))
         self.ball_x = 0
         self.paddle_x = 0
-
+        inc = 0
         while True:
             x = self.execute_opcode(reset_pointer=False)
             y = self.execute_opcode(reset_pointer=False)
@@ -27,26 +28,38 @@ class Arcade(Day):
             if z == 4:
                 self.ball_x = x
                 self.input(-1 if self.ball_x < self.paddle_x else 1 if self.ball_x > self.paddle_x else 0)
+                self.canvas[x, y, inc] = z
+                inc += 1
             if z == 3:
                 self.paddle_x = x
+                inc += 1
+                
 
             if isinstance(z, Day):
+                print(inc)
                 break
             if x == -1 and y == 0:
                 #print("Score: ", z)
                 self.score = z
-                self.visualize()
+                self.scores[inc:] = z
+                #self.visualize()
             else:
-                self.canvas[(x, y)] = z
-                if len(self.canvas.keys()) > 910:
-                    self.visualize()
+                if z in [1,2]:
+                    self.canvas[x, y, :] = z
+                elif z == 0:
+                    self.canvas[x, y, inc:] = z
+                elif z == 3:
+                    self.canvas[x, y, inc] = z
+                    
+                #self.visualize()
+        
         return self
     def visualize(self):
 
         print("Score: ", self.score)
         #x, y = zip(*self.canvas.keys())
-        #painting = {0: " ", 1: "░", 2: "▒", 3: "▉", 4: "◓"}
-        #for j in range(min(y)-2, max(y)+1):
+        painting = {0: " ", 1: "░", 2: "▒", 3: "▉", 4: "◓"}
+        # for j in range(min(y)-2, max(y)+1):
         #    for i in range(min(x), max(x)+1):
         # for j in range(0, 25):
         #     for i in range(0, 45):
@@ -87,27 +100,17 @@ if __name__ == "__main__":
     # 3 is a horizontal paddle tile. The paddle is indestructible.
     # 4 is a ball tile. The ball moves diagonally and bounces off objects.
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(9,5))
 
 
-    def f(x, y):
-        return np.sin(x) + np.cos(y)
-
-    x = np.linspace(0, 2 * np.pi, 120)
-    y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
-    # ims is a list of lists, each row is a list of artists to draw in the
-    # current frame; here we are just animating one artist, the image, in
-    # each frame
     ims = []
-    for i in range(60):
-        x += np.pi / 15.
-        y += np.pi / 20.
-        im = plt.imshow(f(x, y), animated=True)
+    for i in range(0,7000,10):
+        im = plt.imshow(np.rot90(part2.canvas[:,:,i], k=-1), animated=True, cmap=plt.cm.get_cmap('Greens', 6))
         ims.append([im])
-
-    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+    plt.axis('off')
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=False,
                                     repeat_delay=1000)
 
-    # ani.save('dynamic_images.mp4')
+    ani.save('dynamic_images.mp4')
 
     plt.show()
