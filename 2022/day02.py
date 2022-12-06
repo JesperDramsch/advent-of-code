b639ed9218c70cc6e88bf19fbbcbceef22b9f4ff
Jesper Dramsch <jesper@dramsch.net>
2022-12-06 01:27:48 +0100

    Refactor

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,52 +1,57 @@
 from util import Day
 from aocd import submit
 
-def winner(player1, player2):
-    winner = {"scissors": "paper", "paper": "rock", "rock": "scissors"}
-    if winner[player1] == player2:
+
+def winner(player):
+    return {"scissors": "paper", "paper": "rock", "rock": "scissors"}[player]
+
+
+def winner_points(outcome):
+    return {"win": 6, "draw": 3, "lose": 0}[outcome]
+
+
+def selection_points(player):
+    return {"rock": 1, "paper": 2, "scissors": 3}[player]
+
+
+def decode(data, part=1):
+    player1, player2 = data.split(" ")
+
+    code1 = {"A": "rock", "B": "paper", "C": "scissors"}
+    if part == 1:
+        code2 = {"X": "rock", "Y": "paper", "Z": "scissors"}
+    elif part == 2:
+        code2 = {"X": "lose", "Y": "draw", "Z": "win"}
+
+    return code1[player1], code2[player2]
+
+
+def faceoff(player1, player2):
+    if winner(player2) == player1:
         return winner_points("win")
     elif player1 == player2:
         return winner_points("draw")
     else:
         return winner_points("lose")
 
-def winner_points(outcome):
-    return {"win": 6, "draw": 3, "lose": 0}[outcome]
-    
-def selection_points(player1):
-    return {"rock": 1, "paper": 2, "scissors": 3}[player1]
-    
-def decode_part1(data):
-    player1, player2 = data.split(" ")
 
-    code1 = {"A": "rock", "B": "paper", "C": "scissors"}
-    code2 = {"X": "rock", "Y": "paper", "Z": "scissors"}
+def strategic_play(player, outcome):
+    if outcome == "win":
+        play = winner(winner(player))
+    elif outcome == "draw":
+        play = player
+    elif outcome == "lose":
+        play = winner(player)
+    return play
 
-    return code2[player2], code1[player1]
-
-def decode_part2(data):
-    player1, outcome = data.split(" ")
-
-    code1 = {"A": "rock", "B": "paper", "C": "scissors"}
-    code2 = {"X": "lose", "Y": "draw", "Z": "win"}
-
-    winner = {"scissors": "paper", "paper": "rock", "rock": "scissors"}
-    if code2[outcome] == "win":
-        play = winner[winner[code1[player1]]]
-    elif code2[outcome] == "draw":
-        play = code1[player1]
-    elif code2[outcome] == "lose":
-        play = winner[code1[player1]]
-
-    return play, code2[outcome]
 
 def main(day, part=1):
+    day.data = (decode(line, part=part) for line in day.data)
     if part == 1:
-        day.data = (decode_part1(line) for line in day.data)
-        return sum(winner(*line) + selection_points(line[0]) for line in day.data)
+        return sum(faceoff(*line) + selection_points(line[1]) for line in day.data)
     if part == 2:
-        day.data = (decode_part2(line) for line in day.data)
-        return sum(winner_points(line[1]) + selection_points(line[0]) for line in day.data)
+        return sum(selection_points(strategic_play(*line)) + winner_points(line[1]) for line in day.data)
+
 
 if __name__ == "__main__":
     day = Day(2)
