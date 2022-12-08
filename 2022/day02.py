151ffabc9e6e7b476ce724a4a03cc8426bcafe3c
Jesper Dramsch <jesper@dramsch.net>
2022-12-08 23:36:16 +0100

    Refactor into three hashmaps in a trenchcoat

--- 2022/day02.py
+++ 2022/day02.py
@@ -2,16 +2,32 @@ from util import Day
 from aocd import submit
 
 
-def winner(player):
-    return {"scissors": "paper", "paper": "rock", "rock": "scissors"}[player]
+class TwoWayDict(dict):
+    """Two-way dictionary"""
 
+    def __init__(self, dictionary=None):
+        if dictionary is not None:
+            self.from_dict(dictionary)
 
-def winner_points(outcome):
-    return {"win": 6, "draw": 3, "lose": 0}[outcome]
+    def __setitem__(self, key, value):
+        # Remove any previous connections with these values
+        if key in self:
+            del self[key]
+        if value in self:
+            del self[value]
+        dict.__setitem__(self, key, value)
+        dict.__setitem__(self, value, key)
 
+    def __delitem__(self, key):
+        dict.__delitem__(self, self[key])
+        dict.__delitem__(self, key)
 
-def selection_points(player):
-    return {"rock": 1, "paper": 2, "scissors": 3}[player]
+    def __len__(self):
+        return dict.__len__(self) // 2
+
+    def from_dict(self, dictionary):
+        for key, value in dictionary.items():
+            self[key] = value
 
 
 def decode(data, part=1):
@@ -26,31 +42,28 @@ def decode(data, part=1):
     return code1[player1], code2[player2]
 
 
-def faceoff(player1, player2):
-    if winner(player2) == player1:
-        return winner_points("win")
-    elif player1 == player2:
-        return winner_points("draw")
-    else:
-        return winner_points("lose")
+def winner(player):
+    return {"scissors": "paper", "paper": "rock", "rock": "scissors"}[player]
 
 
-def strategic_play(player, outcome):
-    if outcome == "win":
-        play = winner(winner(player))
-    elif outcome == "draw":
-        play = player
-    elif outcome == "lose":
-        play = winner(player)
-    return play
+def winner_points(outcome):
+    return {"win": 6, "draw": 3, "lose": 0}[outcome]
+
+
+def selection_points(player):
+    return {"rock": 1, "paper": 2, "scissors": 3}[player]
+
+
+def game(opponent, result):
+    return TwoWayDict({opponent: "draw", winner(opponent): "lose", winner(winner(opponent)): "win"})[result]
 
 
 def main(day, part=1):
     day.data = (decode(line, part=part) for line in day.data)
     if part == 1:
-        return sum(faceoff(*line) + selection_points(line[1]) for line in day.data)
+        return sum(selection_points(line[1]) + winner_points(game(*line)) for line in day.data)
     if part == 2:
-        return sum(selection_points(strategic_play(*line)) + winner_points(line[1]) for line in day.data)
+        return sum(selection_points(game(*line)) + winner_points(line[1]) for line in day.data)
 
 
 if __name__ == "__main__":
