f894bcfcf3fccec0d893853709dd1d76ea1a8b47
Jesper Dramsch <jesper@dramsch.net>
2022-12-11 16:28:40 +0100

    Template

---
+++ .templates/day.py
@@ -0,0 +1,34 @@
+import sys
+import pytest
+
+sys.path.insert(0, ".")
+from util import Day
+from day{number:02d} import *
+
+@pytest.fixture(scope="function")
+def example():
+    day = Day({number})
+    data = """"""
+
+    day.load(data)
+    return day
+
+@pytest.fixture(scope="function")
+def day():
+    day = Day({number})
+    day.load()
+    return day
+
+## Part 1
+def test_example(example):
+    assert main(example, part=1) == False
+
+def test_part1(day):
+    assert main(day, part=1) == False
+
+## Part 2
+def test_example_p2(example):
+    assert main(example, part=2) == False
+
+def test_part2(day):
+    assert main(day, part=2) == False
---
+++ .templates/test.py
@@ -0,0 +1,22 @@
+from util import Day
+from aocd import submit
+
+def main(day, part=1):
+    if part == 1:
+        pass
+    if part == 2:
+        pass
+
+if __name__ == "__main__":
+    day = Day({number})
+    day.download()
+
+    day.load()
+    p1 = main(day)
+    print(p1)
+    # submit(p1, part="a", day={number}, year={year})
+
+    # day.load()
+    # p2 = main(day, part=2)
+    # print(p2)
+    # submit(p2, part="b", day={number}, year={year})
