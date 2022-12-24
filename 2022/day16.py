89e32bcc707703bdb35eaa9d65ff648a0540fec1
Jesper Dramsch <jesper@dramsch.net>
2022-12-24 15:44:00 +0100

    Works in theory but too slow

--- 2022/day16.py
+++ 2022/day16.py
@@ -1,11 +1,13 @@
 from day import Day
 from aocd import submit
-from functools import lru_cache
+from functools import cache
 from collections import abc, deque
 from dataclasses import dataclass
 
+from cProfile import run
+
 class FrozenDict(abc.Mapping):
-    """Don't forget the docstrings!!"""
+    """A hashable dictionary that is immutable once created."""
     
     def __init__(self, *args, **kwargs):
         self._d = dict(*args, **kwargs)
@@ -110,19 +112,67 @@ def compress_valves(valves):
     
     return valves
 
-@lru_cache(maxsize=None)
+@cache
 def tunneling(valves, opened=(), valve="AA", minutes=30):
     
     if minutes <= 0:
         return 0
 
+    if len(opened) == len(valves)-1:
+        return 0
+
     max_steam = 0
-    current_steam = (minutes -1) * valves[valve].flow
+    minutes -= 1
+    current_steam = 0
+    
+    if valve not in opened:
+        current_steam = minutes * valves[valve].flow
+        opened = tuple(sorted(opened + (valve,)))
 
     for tunnel, steps in valves[valve].neighbours.items():
-        if valve not in opened and current_steam != 0:
-            max_steam = max(max_steam, current_steam + tunneling(valves, opened + (valve,), valve=tunnel, minutes=minutes - 1))
-        max_steam = max(max_steam, tunneling(valves, opened, valve=tunnel, minutes=minutes - steps - 1))
+        max_steam = max(max_steam, current_steam + tunneling(valves, opened, valve=tunnel, minutes=minutes - steps + (current_steam == 0)))
+    
+    return max_steam
+
+@cache
+def floptunneling(valves, opened=(), all_valves=("AA", "AA"), all_minutes=(26, 26)):
+
+    eleflop = all_minutes[0] > all_minutes[1]
+
+    minutes = all_minutes[eleflop]
+    valve = all_valves[eleflop]
+
+    if max(all_minutes) <= 0:
+        return 0
+
+    if len(opened) == len(valves)-1:
+        return 0
+
+    if minutes <= 0:
+        eleflop = not eleflop
+        minutes = all_minutes[eleflop]
+        valve = all_valves[eleflop]
+
+    # print(all_valves, end="")
+    max_steam = 0
+    current_steam = 0
+    minutes -= 1
+    
+    if valve not in opened:
+        current_steam = minutes * valves[valve].flow
+        opened = tuple(sorted(opened + (valve,)))
+
+    for tunnel, steps in valves[valve].neighbours.items():
+        new_minutes = minutes - steps + (current_steam == 0)
+        if new_minutes < all_minutes[not eleflop]:
+            all_minutes = (new_minutes, all_minutes[not eleflop])
+            all_valves = (tunnel, all_valves[not eleflop])
+        else:
+            all_minutes = (all_minutes[not eleflop], new_minutes)
+            all_valves = (all_valves[not eleflop], tunnel)
+        # all_minutes = all_minutes[:eleflop] + (new_minutes,) + all_minutes[eleflop+1:]
+        # all_valves = all_valves[:eleflop] + (tunnel,) + all_valves[eleflop+1:]
+        max_steam = max(max_steam, current_steam + floptunneling(valves, opened, all_valves=all_valves, all_minutes=all_minutes))
     return max_steam
 
 
@@ -131,13 +181,12 @@ def main(day, part=1):
     day.parse_regex(regex, typing=(str, int, str))
     day.data = parse(day.data)
     day.data = compress_valves(day.data)
-    print(day.data)
     if part == 1:
         # return day.data
         return tunneling(day.data)
     if part == 2:
         # return elephant_tunneling(day.data)
-        pass
+        return floptunneling(day.data)
 
 
 if __name__ == "__main__":
@@ -145,23 +194,23 @@ if __name__ == "__main__":
     day.download()
 
     day.load()
-    data = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-Valve BB has flow rate=13; tunnels lead to valves CC, AA
-Valve CC has flow rate=2; tunnels lead to valves DD, BB
-Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
-Valve FF has flow rate=0; tunnels lead to valves EE, GG
-Valve GG has flow rate=0; tunnels lead to valves FF, HH
-Valve HH has flow rate=22; tunnel leads to valve GG
-Valve II has flow rate=0; tunnels lead to valves AA, JJ
-Valve JJ has flow rate=21; tunnel leads to valve II"""
-
-    day.load(data)
-    p1 = main(day)
-    print(p1)
+    # p1 = main(day)
+    # print(p1)
     # submit(p1, part="a", day=16, year=2022)
 
-    # day.load()
-    # p2 = main(day, part=2)
-    # print(p2)
+    day.load()
+#     data = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+# Valve BB has flow rate=13; tunnels lead to valves CC, AA
+# Valve CC has flow rate=2; tunnels lead to valves DD, BB
+# Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+# Valve EE has flow rate=3; tunnels lead to valves FF, DD
+# Valve FF has flow rate=0; tunnels lead to valves EE, GG
+# Valve GG has flow rate=0; tunnels lead to valves FF, HH
+# Valve HH has flow rate=22; tunnel leads to valve GG
+# Valve II has flow rate=0; tunnels lead to valves AA, JJ
+# Valve JJ has flow rate=21; tunnel leads to valve II"""
+
+#     day.load(data)
+    p2 = main(day, part=2)
+    print(p2)
     # submit(p2, part="b", day=16, year=2022)
